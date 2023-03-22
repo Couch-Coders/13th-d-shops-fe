860efc0076a346c4df2da41e11a462f573b1fa58
tkrkr55 <108445626+tkrkr55@users.noreply.github.com>
2023-03-22 13:37:22 +0900

    첫 커밋

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import signIn from '../service/authservice'
+import signIn, { onGetMyInfo, onUserPut1 } from '../service/authservice'
 import { googleSignIn } from '../service/firebaseAuth'
 
 export default function Login() {
   return (
     <div>
       <button onClick={googleSignIn}>로그인</button>
+      <button onClick={onGetMyInfo}>내정보</button>
+      <button onClick={onUserPut1}>수정</button>
     </div>
   )
 }
--- src/service/authservice.js
+++ src/service/authservice.js
@@ -1,10 +1,8 @@
 import axios from 'axios'
 
 
-
-
 const signIn = async ({token}) => {
-  const response = await axios.get('/users/me', {
+  const response = await axios.post('/auth',{}, {
     headers: { Authorization: `${token}` },
     withCredentials: true,
 
@@ -14,7 +12,36 @@ const signIn = async ({token}) => {
  return response.data
  
 }
+export const onGetMyInfo = async () => {
+  const response1 = await axios.get("/users/me");
+   console.log(response1.data);
+ };
 
+
+ export const onUserPut1 = async () => {
+  const responsePut1 = await axios.put(
+    "/users/me",
+    {
+      name: "name-1",
+      phone: "phone-1",
+      company: {
+        name: "company-name-1",
+        phone: "company-phone-1",
+        email: "company-email-1",
+        address: {
+          name: "company-address-name-1",
+        },
+      },
+    },
+    {
+      withCredentials: true,
+    }
+  );
+  console.log(responsePut1.data);
+
+  const response1 = await axios.get("/users/me");
+  console.log(response1.data);
+};
 
 const signOut = async () => {
   const response = await axios.delete('/auth', {
