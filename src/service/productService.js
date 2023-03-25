87a7013176527060231564991be3fcb25c0814d0
jayleeforever <jayforgood@gmail.com>
2023-03-26 00:19:58 +0900

    add create product #11

--- src/service/productService.js
+++ src/service/productService.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
 export const onProductPut1 = async (inputs) => {
-  const responsePut1 = await axios.post("/users/me/products", {
-    name: "name",
-    title: "title",
-    options: "optoins",
-    description: "inputs.description",
+  const responsePost = await axios.post("/users/me/products", {
+    title: inputs.title,
+    options: inputs.options,
+    description: inputs.description,
+    //이건 내정보에서 가져오기
     location_x: 126.777966,
     location_y: 37.366536,
   });
-  console.log(responsePut1.data);
+  console.log(responsePost.data);
 
-  const response1 = await axios.get("/products");
-  console.log(response1.data);
+  //받아온 자료 확인하기
+  const responseGet = await axios.get("/products/" + responsePost.data.seq);
+  console.log(responseGet.data);
 };
