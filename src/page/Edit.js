e35660137b82a9f7d19b01cf3ee70f5b6f586ddb
jayleeforever <jayforgood@gmail.com>
2023-03-26 03:25:00 +0900

    add 상품등록 #12, 상품수정 #13, 상품 삭제

--- src/page/Edit.js
+++ src/page/Edit.js
@@ -1,48 +1,120 @@
 import React, { useState } from "react";
-import { onProductPut1 } from "../service/productService";
+import {
+  onProductPost,
+  onProductGet,
+  onProductPut,
+  onProductDelete,
+} from "../service/productService";
+import { useParams } from "react-router-dom";
 
 export default function Edit() {
+  let { params } = useParams();
+  console.log(params);
   const [inputs, setInputs] = useState({});
   const handleChange = (e) => {
     const { name, value } = e.target;
     setInputs((inputs) => ({ ...inputs, [name]: value }));
   };
-  const handleSubmit = (e) => {
+  const handleSubmitPost = (e) => {
     e.preventDefault();
-    onProductPut1(inputs);
+    onProductPost(inputs);
+  };
+  const handleSubmitPut = (e) => {
+    e.preventDefault();
+    onProductPut(inputs);
+  };
+
+  const handleSubmitDelete = (e) => {
+    e.preventDefault();
+    onProductDelete(inputs);
+  };
+
+  const handleSubmitButton = (e) => {
+    e.preventDefault();
+    onProductGet(inputs);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <div>
       <div>
-        <div>{/* <img></img> */}</div>
-        <div>
-          <div>옵션</div>
-          <input
-            type="text"
-            name="options"
-            value={inputs.options ?? ""}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <div>
-          <p>글제목</p>
-          <input
-            type="text"
-            name="title"
-            value={inputs.title ?? ""}
-            onChange={handleChange}
-          ></input>
-          <p> 내용</p>
-          <input
-            type="text"
-            name="description"
-            value={inputs.description ?? ""}
-            onChange={handleChange}
-          ></input>
-        </div>
+        <form onSubmit={handleSubmitPost}>
+          <div>
+            <div>{/* <img></img> */}</div>
+            <div>
+              <div>옵션</div>
+              <input
+                type="text"
+                name="options"
+                value={inputs.options ?? ""}
+                onChange={handleChange}
+              ></input>
+            </div>
+            <div>
+              <p>글제목</p>
+              <input
+                type="text"
+                name="title"
+                value={inputs.title ?? ""}
+                onChange={handleChange}
+              ></input>
+              <p> 내용</p>
+              <input
+                type="text"
+                name="description"
+                value={inputs.description ?? ""}
+                onChange={handleChange}
+              ></input>
+            </div>
+          </div>
+          <button>등록</button>
+        </form>
       </div>
-      <button>등록</button>
-    </form>
+      <div>
+        <div>seq</div>
+        <input
+          type="text"
+          name="seq"
+          value={inputs.seq ?? ""}
+          onChange={handleChange}
+        ></input>
+        <button onClick={handleSubmitButton}>
+          불러오기후 화면뿌리기 해주세요
+        </button>
+        <button onClick={handleSubmitDelete}>삭제</button>
+      </div>
+      <div>
+        <form onSubmit={handleSubmitPut}>
+          <div>
+            <div>{/* <img></img> */}</div>
+            <div>
+              <div>옵션</div>
+              <input
+                type="text"
+                name="options"
+                value={inputs.options ?? ""}
+                onChange={handleChange}
+              ></input>
+            </div>
+            <div>
+              <p>글제목</p>
+              <input
+                type="text"
+                name="title"
+                value={inputs.title ?? ""}
+                onChange={handleChange}
+              ></input>
+              <p> 내용</p>
+              <input
+                type="text"
+                name="description"
+                value={inputs.description ?? ""}
+                onChange={handleChange}
+              ></input>
+            </div>
+          </div>
+          <button>수정</button>
+        </form>
+      </div>
+    </div>
   );
 }
--- src/service/productService.js
+++ src/service/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-
-export const onProductPut1 = async (inputs) => {
+// 20230326 jay 품목 등록
+export const onProductPost = async (inputs) => {
   const responsePost = await axios.post("/users/me/products", {
     title: inputs.title,
     options: inputs.options,
@@ -14,4 +14,32 @@ export const onProductPut1 = async (inputs) => {
   //받아온 자료 확인하기
   const responseGet = await axios.get("/products/" + responsePost.data.seq);
   console.log(responseGet.data);
+};
+
+// 20230326 jay 품목 읽기
+export const onProductGet = async (inputs) => {
+  const response = await axios.get("/products/" + inputs.seq);
+  console.log(response.data);
+  inputs.seq = response.data.seq;
+  inputs.title = response.data.title;
+  inputs.options = response.data.options;
+  inputs.description = response.data.description;
+  inputs.location_x = response.data.location_x;
+  inputs.location_y = response.data.location_y;
+};
+
+// 20230326 jay 품목 수정
+export const onProductPut = async (inputs) => {
+  const response = await axios.put("/users/me/products/" + inputs.seq, {
+    name: inputs.name,
+    title: inputs.title,
+    options: inputs.options,
+    description: inputs.description,
+  });
+  console.log(response.data);
+};
+
+export const onProductDelete = async (inputs) => {
+  const response = await axios.delete("/users/me/products/" + inputs.seq);
+  console.log(response.data);
 };
