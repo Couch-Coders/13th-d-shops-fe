6de01cba393f32f1b7a03f3ebcd48b77d436d226
jayleeforever <jayforgood@gmail.com>
2023-03-27 08:07:49 +0900

    페이지 처음에 데이터 불러오기(MainPage)#23

--- src/service/MainPageService.js
+++ src/service/MainPageService.js
@@ -2,10 +2,17 @@
 import axios from "axios";
 
 // 20230326 by jay 가까운 상품 목록 가져오기
-export const onProductNearListServiceGet = async (inputs) => {
+const getProductNear = async (location_x, location_y) => {
   const response = await axios.get(
-    "/products/near?location_x=126.977966&location_y=36.566537&page=0&size=5"
+    "/products/near?location_x=" +
+      location_x +
+      "&location_y=" +
+      location_y +
+      "&page=0&size=5"
   );
-  console.log("onProductNearListServiceGet", response.data);
-  inputs.data = response.data.content;
+  return response.data;
 };
+
+const MainPageService = { getProductNear };
+
+export default MainPageService;
