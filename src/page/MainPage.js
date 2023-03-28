12068fb33db4150c48b96abf433fb54f9af118b3
jayleeforever <jayforgood@gmail.com>
2023-03-28 14:58:50 +0900

    디자인 작업중

--- src/page/MainPage.js
+++ src/page/MainPage.js
@@ -1,8 +1,10 @@
 import Carousel from "../component/Carousel";
 import React, { useEffect, useState } from "react";
 import MainPageService from "../service/MainPageService";
-import { Card, List } from "antd";
+import { Card, List, Avatar, Descriptions } from "antd";
 import { useSelector } from "react-redux";
+
+const { Meta } = Card;
 
 export default function MainPage() {
   const user = useSelector((state) => state.user.user);
@@ -23,33 +25,33 @@ export default function MainPage() {
     script.onload = () => {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          // 지도
-          var mapContainer = document.getElementById("map"), // 지도를 표시할 div
-            mapOption = {
-              center: new window.kakao.maps.LatLng(
-                userLocation.latitude,
-                userLocation.longitude
-              ), // 지도의 중심좌표
-              level: 3, // 지도의 확대 레벨
-            };
+          // 20230327 jay 지도
+          // var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+          //   mapOption = {
+          //     center: new window.kakao.maps.LatLng(
+          //       userLocation.latitude,
+          //       userLocation.longitude
+          //     ), // 지도의 중심좌표
+          //     level: 3, // 지도의 확대 레벨
+          //   };
 
-          var map = new window.kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+          // var map = new window.kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-          var iwContent = '<div style="padding:5px;">내위치</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-            iwPosition = new window.kakao.maps.LatLng(
-              userLocation.latitude,
-              userLocation.longitude
-            ), //인포윈도우 표시 위치입니다
-            iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+          // var iwContent = '<div style="padding:5px;">내위치</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+          //   iwPosition = new window.kakao.maps.LatLng(
+          //     userLocation.latitude,
+          //     userLocation.longitude
+          //   ), //인포윈도우 표시 위치입니다
+          //   iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
 
-          // 인포윈도우를 생성하고 지도에 표시합니다
-          var infowindow = new window.kakao.maps.InfoWindow({
-            map: map, // 인포윈도우가 표시될 지도
-            position: iwPosition,
-            content: iwContent,
-            removable: iwRemoveable,
-          });
-
+          // // 인포윈도우를 생성하고 지도에 표시합니다
+          // var infowindow = new window.kakao.maps.InfoWindow({
+          //   map: map, // 인포윈도우가 표시될 지도
+          //   position: iwPosition,
+          //   content: iwContent,
+          //   removable: iwRemoveable,
+          // });
+          // 20230327 jay 위치
           setUserLocation({
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
@@ -89,13 +91,14 @@ export default function MainPage() {
       <div>
         <Carousel />
       </div>
-      {userLocation && (
+      {/* {userLocation && (
         <div>
           <p>Latitude: {userLocation.latitude}</p>
           <p>Longitude: {userLocation.longitude}</p>
           <div id="map" style={{ width: "500px", height: "400px" }}></div>
         </div>
-      )}
+      )} */}
+      <Descriptions title="최신상품"></Descriptions>
       <List
         grid={{
           gutter: 16,
@@ -103,13 +106,29 @@ export default function MainPage() {
           sm: 2,
           md: 4,
           lg: 4,
-          xl: 6,
-          xxl: 3,
+          xl: 5,
+          xxl: 5,
         }}
         dataSource={productNear}
         renderItem={(item) => (
           <List.Item>
-            <Card title={item.title}>{item.description}</Card>
+            <Card
+              style={{
+                width: 200,
+                height: 300,
+              }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                />
+              }
+              actions={[]}
+            >
+              <Meta title={item.title} />
+              <p>성남시 중원구</p>
+              <Meta description={item.options} />
+            </Card>
           </List.Item>
         )}
       />
