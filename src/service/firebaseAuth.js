0c07bd218418a9f34048f8a94fcc5b3132009b76
tkrkr55 <108445626+tkrkr55@users.noreply.github.com>
2023-04-04 23:27:15 +0900

    env 이름 수정

--- src/service/firebaseAuth.js
+++ src/service/firebaseAuth.js
@@ -7,12 +7,12 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FITEBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FITEBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FITEBASE_PRODUCTID,
-  storageBucket: process.env.REACT_APP_FITEBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FITEBASE_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FITEBASE_APPID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PRODUCTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
   measurementId: process.env.REACT_APP_MEASUREMENTID,
 };
 
