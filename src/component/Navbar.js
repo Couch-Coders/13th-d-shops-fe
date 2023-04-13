31e3856d3d2d46ff0c334c294b8e4b65bde00b2d
jayleeforever <jayforgood@gmail.com>
2023-04-13 17:12:13 +0900

    fix 첫 페이지 접속 후 깜박거림#4

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,24 +5,32 @@ import { googleSignIn, googleSignOut } from "../service/firebaseAuth";
 import User from "./User";
 
 export default function Navbar() {
-
   const user = useSelector((state) => state.user.user);
-  
+  const loading = useSelector((state) => state.user.loading);
+
   return (
     <div className="header_right">
       <Link to="/products" className="product_list_text">
         상품목록
       </Link>
-      {user && <User user={user} />}
-      {user && (
-        <button className="login_btn" onClick={googleSignOut}>
-          로그아웃
-        </button>
+      {loading ? <div></div> : user && <User user={user} />}
+      {loading ? (
+        <div></div>
+      ) : (
+        user && (
+          <button className="login_btn" onClick={googleSignOut}>
+            로그아웃
+          </button>
+        )
       )}
-      {!user && (
-        <button className="login_btn" onClick={googleSignIn}>
-          로그인
-        </button>
+      {loading ? (
+        <div></div>
+      ) : (
+        !user && (
+          <button className="login_btn" onClick={googleSignIn}>
+            로그인
+          </button>
+        )
       )}
     </div>
   );
